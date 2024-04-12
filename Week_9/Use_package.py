7477902491d5b1b256383b8b5697231aa35eccfc
RajLaxmi05 <162013862+RajLaxmi05@users.noreply.github.com>
2024-04-12 11:29:27 +0530

    Create Use_package.py

---
+++ Week_9/Use_package.py
@@ -0,0 +1,8 @@
+from my_math_package.arithmetic import subtract, divide
+from my_math_package.geometry import calculate_area_circle, PI, calculate_perimeter_rectangle, calculate_circumference_circle
+
+print(subtract(5, 3))
+print(divide(16, 4))
+print(calculate_area_circle(5))
+print(calculate_perimeter_rectangle(5, 3))
+print(calculate_circumference_circle(4))
