175d551a27a99998dd370d4f1ad4681fbcb5b15f
RajLaxmi05 <162013862+RajLaxmi05@users.noreply.github.com>
2024-04-12 11:27:04 +0530

    Create setup.py

---
+++ Week_9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-math-package',
+    version='0.1',
+    author='Raj Laxmi',
+    description='A package with basic math and geometry functions',
+    packages=find_packages(),
+)
