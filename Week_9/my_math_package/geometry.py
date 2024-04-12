fc70c03e9eeaa5912078438aafa068ba18ebdf06
RajLaxmi05 <162013862+RajLaxmi05@users.noreply.github.com>
2024-04-12 11:24:34 +0530

    Create geometry.py

---
+++ Week_9/my_math_package/geometry.py
@@ -0,0 +1,13 @@
+PI = 3.14
+
+def calculate_area_rectangle(length, width):
+    return length * width
+
+def calculate_area_circle(radius):
+    return PI * (radius ** 2)
+
+def calculate_perimeter_rectangle(length, width):
+    return 2 * (length + width)
+
+def calculate_circumference_circle(radius):
+    return 2 * PI * radius
