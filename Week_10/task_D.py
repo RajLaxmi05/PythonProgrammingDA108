5b164146790a16c8f576bca03708cb1489b4940e
RajLaxmi05 <rlaxmi2111@gmail.com>
2024-06-19 22:15:05 +0530

    fix new bug

--- Week_10/task_D.py
+++ Week_10/task_D.py
@@ -1,4 +1,8 @@
 import matplotlib.pyplot as plt
+import pandas as pd 
+
+# Assuming country_data is already defined
+country_data = pd.read_csv('WHO-COVID-19-global-data.csv')  # Example of reading the data
 df = country_data[['Country', 'Cumulative_cases', 'Cumulative_deaths']]
 plt.figure(figsize=(10, 6))
 plt.hist(df['Cumulative_cases'], bins=20, color='blue', edgecolor='r')
@@ -7,3 +11,4 @@ plt.ylabel('Frequency')
 plt.title('Histogram of Cumulative COVID-19 Cases')
 plt.grid(True)
 plt.show()
+
